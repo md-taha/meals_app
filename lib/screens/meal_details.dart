db9a97a7a9b4a1f0c403939048b2a1574d085351
md-taha <mohammadtaha9935@gmail.com>
2024-02-26 12:15:56 +0530

    Added animation to favorite marking star.

--- lib/screens/meal_details.dart
+++ lib/screens/meal_details.dart
@@ -68,7 +68,20 @@ class MealDetailsScreen extends ConsumerWidget {
                 );
               },
               // icon: const Icon(Icons.star),
-              icon: Icon(isFavorite ? Icons.star : Icons.star_border),
+              icon: AnimatedSwitcher(
+                duration: const Duration(milliseconds: 300),
+                transitionBuilder: (child, animation) {
+                  return RotationTransition(
+                    // turns: animation,
+                    turns: Tween<double>(begin: 0.5, end: 1).animate(animation),
+                    child: child,
+                  );
+                },
+                child: Icon(
+                  isFavorite ? Icons.star : Icons.star_border,
+                  key: ValueKey(isFavorite),
+                ),
+              ),
             ),
           ],
         ),
