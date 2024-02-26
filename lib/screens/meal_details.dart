9678ffa5448f749fe63d2fb43dea29bb6d14ded3
md-taha <mohammadtaha9935@gmail.com>
2024-02-26 11:07:18 +0530

    Finished with providers

--- lib/screens/meal_details.dart
+++ lib/screens/meal_details.dart
@@ -44,6 +44,8 @@ class MealDetailsScreen extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final favoriteMeals = ref.watch(favoriteMealsProvider);
+    final isFavorite = favoriteMeals.contains(meal);
     return Scaffold(
         appBar: AppBar(
           title: Text(meal.title),
@@ -65,7 +67,8 @@ class MealDetailsScreen extends ConsumerWidget {
                   ),
                 );
               },
-              icon: const Icon(Icons.star),
+              // icon: const Icon(Icons.star),
+              icon: Icon(isFavorite ? Icons.star : Icons.star_border),
             ),
           ],
         ),
